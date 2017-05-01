c5099216b1083f61b67929b6eca67413883015ce
Matthias.will@iubh-fernstudium.de <Studium2015>
2017-05-01 19:35:50 +0200

    StatusID per GET übermitteln

--- cKorrekturDetail.php
+++ cKorrekturDetail.php
@@ -1,54 +1,45 @@
 <?php
     require_once('model.php'); // Model-Klasse für DB Operationen aufrufen
     
+    //Wenn per GET Parameter eine KorrekturID übermittelt wurde
+    //DANN: Setze Sessionvariablen + Neu Aufrufen des Scripts
     if(isset($_GET['korrekturid'])) {
         $_SESSION['korrekturid'] = $_GET['korrekturid'];
-        $_SESSION['status'] = $_GET['status'];
+        $_SESSION['status'] = $_GET['statusid'];
         header("location: vKorrekturDetail.php");
-
-
-
     }
-    
-    if(isset($_GET['back'])) {
-        unset($_SESSION['korrekturid']);
-        unset($_SESSION['status']);
-        header("location: vKorrekturOverview.php");
-        
-    }
-    
+
+    //Wenn der Speicherbutton in der Detailkorrektur geklickt wurde!
     if(isset($_GET['save'])) {
-        //echo '##Status: '.$_GET['status'].' - Grund: '.$_GET['reason'].' ##';
         $reason = $_GET['reason'];
         $NewStatus = $_GET['status'];
         $OldStatus = $_SESSION['status'];
         $korrekturid = $_SESSION['korrekturid'];
+        $statustext = "";
         
-        //Wenn der Status geändert wurde wird dieser in der Korrektur geändert und außerdem eine neue Bearbeitung gesetzt
+        //Wenn der Status explizit geändert wurde wird dieser in der Korrektur geändert
+        //Außerdem gibt es eine Mitteltung
         if ($NewStatus != $_SESSION['status']) {
-            $statustext = "Status geändert: $OldStatus &rarr; $NewStatus";
+            $statustext .= "Status geändert: $OldStatus &rarr; $NewStatus\r\n";
             
-            //neue Bearbeitung für Statusänderung schreiben
-            $BearbeitungStatus = new db(); // Erstelle ein neues Object, Klasse db()
-            $insert = $BearbeitungStatus->execute("INSERT INTO bearbeitung  (text, korrekturID)
-                                                  VALUES('$statustext', '$korrekturid')");            
-                        
             //Status der Korrektur ändern
             $UpdateKorrektur = new db(); // Erstelle ein neues Object, Klasse db()
             $update = $UpdateKorrektur->execute("UPDATE korrektur set statusID = $NewStatus where ID = $korrekturid"); 
         }
         
+        $statustext .= "Text: ".$reason;
         $BearbeitungSubmit = new db(); // Erstelle ein neues Object, Klasse db()
         //Daten in DB schreiben
         $insert = $BearbeitungSubmit->execute("INSERT INTO bearbeitung  (text, korrekturID)
-                                                  VALUES('$reason', '$korrekturid')");
-        
+                                                  VALUES('$statustext', '$korrekturid')");
         
         unset($_SESSION['korrekturid']);
         unset($_SESSION['status']);
         header("location: vKorrekturOverview.php");
     }
     
+    
+    //Abfrage + Anzeige der Tabelle aller bereits durchgeführten Bearbeitungen der Korrektur
     function tabelle() {
     
         if ($_SESSION['korrekturid'] != "" ) {
@@ -56,20 +47,20 @@
         echo "<hr />"
             . "<h2>Historie</h2>";
         
-        //hier kommen alle bearbeitungstände zur korrektur hinein
-        $AlleBearbeitungen = new db(); // Erstelle ein neues Object, Klasse db()
         
+        //SQL Query
+        $AlleBearbeitungen = new db(); // Erstelle ein neues Object, Klasse db()
           $Bearbeitungen = $AlleBearbeitungen->GetAll(""
           . "select * "
           . "from bearbeitung "
           . "where korrekturID =  ".$_SESSION['korrekturid']." "
           . "order by id DESC");
           
+          //HTML Generierung
           if (count($Bearbeitungen)>0) {
               echo '<div class="container"><table class="table table-bordered">
                             <thead>
                              <tr>
-                               <th>ID.</th>
                                <th>Datum</th>
                                <th>Text</th>
                              </tr>
@@ -82,9 +73,8 @@
                     echo '  
                             <tbody>
                                 <tr>
-                                <td>'.$value['ID'].'</td>
                                 <td>'.$value['date'].'</td>
-                                <td>'.$value['text'].'</td>
+                                <td>'.nl2br($value['text']).'</td>
                                 </td>
                              </tr>
                            </tbody>
