f9cfc53f5c89f423c55f72bd3901af48cb30f85a
Matthias.will@iubh-fernstudium.de <Studium2015>
2017-05-01 20:03:58 +0200

    einzelne Sessionvariablen zurücksetzen

--- cKorrekturOverview.php
+++ cKorrekturOverview.php
@@ -1,24 +1,28 @@
 <?php
-  require_once('model.php'); // Model-Klasse für DB Operationen aufrufen
-  
-  
-  $GetKorrekturen = new db(); // Erstelle ein neues Object, Klasse db()
-  
-  //Alle Korrekturen des aktiven Dozenten auslesen
-  $korrekturen = $GetKorrekturen->GetAll(""
-          . "select KOR.ID,KOR.description,STA.ID as 'statusid',STA.status,MAT.material, MODU.module,TYP.type,USR.email "
-          . "from korrektur KOR "
-          . "inner join material MAT on MAT.ID = KOR.materialID "
-          . "inner join module MODU on MODU.ID = KOR.moduleID "
-          . "inner join type TYP on TYP.ID = KOR.typeID "
-          . "inner join user USR on USR.ID = KOR.userStudentID "
-          . "inner join status STA on STA.ID = KOR.statusID "
-          . "where KOR.userDozentID = ".$_SESSION['userid']."");
-  
-  
-  //Wenn der Dozent Korrekturen hat, diese anzeigen
-  if (count($korrekturen)>0) {
-      echo '<table class="table table-bordered">
+
+require_once('model.php'); // Model-Klasse für DB Operationen aufrufen
+
+//Sessionvariablen löschen, falls diese noch geladen sein sollten von einem KorrekturDetail
+unset($_SESSION['korrekturid']);
+unset($_SESSION['status']);
+
+
+$GetKorrekturen = new db(); // Erstelle ein neues Object, Klasse db()
+//Alle Korrekturen des aktiven Dozenten auslesen
+$korrekturen = $GetKorrekturen->GetAll(""
+        . "select KOR.ID,KOR.description,STA.ID as 'statusid',STA.status,MAT.material, MODU.module,TYP.type,USR.email "
+        . "from korrektur KOR "
+        . "inner join material MAT on MAT.ID = KOR.materialID "
+        . "inner join module MODU on MODU.ID = KOR.moduleID "
+        . "inner join type TYP on TYP.ID = KOR.typeID "
+        . "inner join user USR on USR.ID = KOR.userStudentID "
+        . "inner join status STA on STA.ID = KOR.statusID "
+        . "where KOR.userDozentID = " . $_SESSION['userid'] . "");
+
+
+//Wenn der Dozent Korrekturen hat, diese anzeigen
+if (count($korrekturen) > 0) {
+    echo '<table class="table table-bordered">
                     <thead>
                      <tr>
                        <th>Korrekturnr.</th>
@@ -30,32 +34,31 @@
                        <th>Administration</th>
                      </tr>
                    </thead>';
-      
-      //Alle Daten der DB Abfrage in eine Tabelle schreiben
-        foreach ($korrekturen as $value) 
-        {
 
-            echo '  
+    //Alle Daten der DB Abfrage in eine Tabelle schreiben
+    foreach ($korrekturen as $value) {
+
+        echo '  
                     <tbody>
                         <tr>
-                        <td>'.$value['ID'].'</td>
-                        <td>'.$value['module'].'</td>
-                        <td>'.$value['type'].'</td>
-                        <td>'.$value['material'].'</td>
-                        <td>'.$value['description'].'</td>
-                        <td>'.$value['status'].'</td>
-                        <td><a href="vKorrekturDetail.php?korrekturid='.$value['ID'].'&statusid='.$value['statusid'].'" target="_self"">Bearbeiten</a>
+                        <td>' . $value['ID'] . '</td>
+                        <td>' . $value['module'] . '</td>
+                        <td>' . $value['type'] . '</td>
+                        <td>' . $value['material'] . '</td>
+                        <td>' . $value['description'] . '</td>
+                        <td>' . $value['status'] . '</td>
+                        <td><a href="vKorrekturDetail.php?korrekturid=' . $value['ID'] . '&statusid=' . $value['statusid'] . '" target="_self"">Bearbeiten</a>
                         </td>
                      </tr>
                    </tbody>
                  ';
-        }
-        echo '</table>';
-  }
+    }
+    echo '</table>';
+}
 
-  //Wenn Dozent keine Korrekturen hat Meldung anzeigen
-  else {
+//Wenn Dozent keine Korrekturen hat Meldung anzeigen
+else {
     echo '<br><div class="alert alert-success alert-dismissable">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Keine Korrekturen zur Bearbeitung vorhanden!</div>';
-  }
-  ?>
+}
+?>
