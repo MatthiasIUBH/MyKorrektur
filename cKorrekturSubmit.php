968f8ca8ecbed76a9bf287ce3a169aeb942db482
Matthias.will@iubh-fernstudium.de <Studium2015>
2017-05-13 23:39:11 +0200

    Email Implementierung

--- cKorrekturSubmit.php
+++ cKorrekturSubmit.php
@@ -2,7 +2,9 @@
 
 if(isset($_GET['submit']) && $_SESSION['rolle'] == 'Student') {
 
-        include('model.php'); // Model-Klasse für DB Operationen aufrufen
+        require_once('model.php'); // Model-Klasse für DB Operationen aufrufen
+        require_once('simplemail/class.simple_mail.php'); // Model-Klasse für DB Operationen aufrufen
+
         $KorrekturSubmit = new db(); // Erstelle ein neues Object, Klasse db()
         $GetDozentId = new db(); // Erstelle ein neues Object, Klasse db()
         
@@ -14,12 +16,22 @@ if(isset($_GET['submit']) && $_SESSION['rolle'] == 'Student') {
         $email = $_SESSION['email'];
         $userid = $_SESSION['userid'];
         
-        //per SQL rausfinden welcher Dozent für das jeweilige Modul zuständig ist
-        $dozent = $GetDozentId->getOne("Select userID from module where ID = $module");
+        //per SQL rausfinden welcher Dozent für das jeweilige Modul zuständig ist + seine Mailadresse
+        $dozent = $GetDozentId->getOne("Select MO.userID, USR.email from module MO Inner join user USR on USR.ID = MO.userID where MO.ID = $module");
 
         //Daten in DB schreiben
         $insert = $KorrekturSubmit->execute("INSERT INTO korrektur  (userStudentID, moduleID, typeID, description, materialID, userDozentID, statusID)
                                                   VALUES('$userid', '$module', '$type', '$description', '$material','".$dozent['userID']."',1);");
+        
+        //Email senden an zuständigen Dozenten
+        $mailer = new SimpleMail();
+            $mailer ->setTo($dozent['email'], $dozent['email'])
+                    ->setFrom('info@mykorrektur.de', 'info@mykorrektur.de')
+                    ->setSubject('Neue Korrektur zur Bearbeitung!')
+                    ->setMessage("Es ist eine neue Korrektur vorhanden und muss bearbeitet werden!")
+                    ->setHtml()
+                    ->setWrap(100)
+                    ->send();
         
         //Sessionflag wird gesetzt und ein Redirect wird ausgeführt --> auf der Seite wird dann auch der erfolgreiche DB insert angezeigt
         $_SESSION['submitflag'] = true;
