51fae585c5b44d617868b58c1c6cdf18f10d36a3
Matthias.will@iubh-fernstudium.de <Studium2015>
2017-05-30 21:48:30 +0200

    Logo einbinden

--- vNav.php
+++ vNav.php
@@ -14,7 +14,9 @@ and open the template in the editor.
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="vLogin.php">MyKorrektur</a>
+
+                <a class="navbar-brand" href="">MyKorrektur</a>
+                <img style="margin-top: 7px;" src="IUBH_LOGO.png">
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
